e0b65260d32fdc74123a0fa8c6408b7b7255fc5c
Doug Conyers <doug.conyers@gmail.com>
2025-02-26 14:55:27 -0600

    I removed the led_strip component and coded directly against the rmt (including the example encoder).

--- main/led_strip_test.c
+++ main/led_strip_test.c
@@ -1,47 +1,41 @@
 #include "led_strip_test.h"
 
+#include <string.h>
+
 #include "esp_log.h"
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-#include "led_strip.h"
+#include "driver/rmt_tx.h"
+#include "led_strip_encoder.h"
 
 // ESP_LOG Tag
 static const char *TAG = "LED_Strip";
 
 static TaskHandle_t xHandle = NULL;
 
-
-static led_strip_handle_t led_strip = NULL;
+static uint8_t led_strip_pixels[LED_STRIP_LED_COUNT * 3];
+static rmt_channel_handle_t led_chan;
+static rmt_encoder_handle_t led_encoder;
 
 static void updateLED(int Red, int Green, int Blue)
 {
-    if (led_strip == NULL)
-    {
-        ESP_LOGE(TAG, "LED strip not initialized!");
-        return;
-    }
 
     ESP_LOGD(TAG, "Setting to %02x %02x %02x", Red, Green, Blue);
 
-    if (Red == 0 && Green == 0 && Blue == 0)
-    {
-        ESP_LOGI(TAG, "Setting clear");
-        ESP_ERROR_CHECK(led_strip_clear(led_strip));
-    }
-    else
     {
         for (int i = 0; i < LED_STRIP_LED_COUNT; i++)
         {
-            ESP_ERROR_CHECK(led_strip_set_pixel(led_strip, i, Red, Green, Blue));
+            // ESP_ERROR_CHECK(led_strip_set_pixel(led_strip, i, Red, Green, Blue));
+            led_strip_pixels[i * 3 + 0] = Green;
+            led_strip_pixels[i * 3 + 1] = Red;
+            led_strip_pixels[i * 3 + 2] = Blue;
         }
-        ESP_LOGI(TAG, "calling led_strip_refresh");
-        ESP_ERROR_CHECK(led_strip_refresh(led_strip));
     }
 }
 
-static void controllerTask(void * pvParameters)
+static void controllerTask(void *pvParameters)
 {
     ESP_LOGD(TAG, "controllerTask() called");
     static LEDStatus last_status = OFF;
@@ -68,42 +62,47 @@ static void controllerTask(void * pvParameters)
             break;
         }
 
+        rmt_transmit_config_t tx_config = {
+            .loop_count = 0, // no transfer loop
+        };
+
+        // Flush RGB values to LEDs
+        ESP_ERROR_CHECK(rmt_transmit(led_chan, led_encoder, led_strip_pixels, sizeof(led_strip_pixels), &tx_config));
+        ESP_ERROR_CHECK(rmt_tx_wait_all_done(led_chan, portMAX_DELAY));
+
         last_status = (LEDStatus)((((int)last_status) + 1) % (1 + (int)WHITE));
 
         vTaskDelay(pdMS_TO_TICKS(50)); // Delay for 100 ms
     }
 }
-
-
 esp_err_t led_strip_init(void)
 {
 
     ESP_LOGI(TAG, "Initializing LED strip");
 
-    // LED strip general initialization, according to your led board design
-    led_strip_config_t strip_config = {
-        .strip_gpio_num = LED_STRIP_GPIO_PIN,                        // The GPIO that connected to the LED strip's data line
-        .max_leds = LED_STRIP_LED_COUNT,                             // The number of LEDs in the strip,
-        .led_model = LED_MODEL_SK6812,                               // LED strip model
-        .color_component_format = LED_STRIP_COLOR_COMPONENT_FMT_GRB, // The color order of the strip: GRB
-        .flags = {
-            .invert_out = false, // don't invert the output signal
-        }};
-
-    // LED strip backend configuration: RMT
-    led_strip_rmt_config_t rmt_config = {
-        .clk_src = RMT_CLK_SRC_APB,            // different clock source can lead to different power consumption
-        .resolution_hz = LED_STRIP_RMT_RES_HZ, // RMT counter clock frequency
-        .mem_block_symbols = 64,               // the memory size of each RMT channel, in words (4 bytes)
-        .flags = {
-            .with_dma = false, // DMA feature is available on chips like ESP32-S3/P4
-        }};
-
     esp_log_level_set("rmt", ESP_LOG_DEBUG);
 
     // LED Strip object handle
-    ESP_ERROR_CHECK(led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip));
-    ESP_LOGI(TAG, "Created LED strip object with RMT backend");
+    ESP_LOGI(TAG, "Create RMT TX channel");
+    led_chan = NULL;
+    rmt_tx_channel_config_t tx_chan_config = {
+        .clk_src = RMT_CLK_SRC_DEFAULT, // select source clock
+        .gpio_num = LED_STRIP_GPIO_PIN,
+        .mem_block_symbols = 64, // increase the block size can make the LED less flickering
+        .resolution_hz = LED_STRIP_RMT_RES_HZ,
+        .trans_queue_depth = 4, // set the number of transactions that can be pending in the background
+    };
+    ESP_ERROR_CHECK(rmt_new_tx_channel(&tx_chan_config, &led_chan));
+
+    ESP_LOGI(TAG, "Install led strip encoder");
+    led_encoder = NULL;
+    led_strip_encoder_config_t encoder_config = {
+        .resolution = LED_STRIP_RMT_RES_HZ,
+    };
+    ESP_ERROR_CHECK(rmt_new_led_strip_encoder(&encoder_config, &led_encoder));
+
+    ESP_LOGI(TAG, "Enable RMT TX channel");
+    ESP_ERROR_CHECK(rmt_enable(led_chan));
 
     BaseType_t xReturned;
     xReturned = xTaskCreate(controllerTask,
@@ -125,4 +124,3 @@ esp_err_t led_strip_init(void)
 
     return ESP_OK;
 }
-
